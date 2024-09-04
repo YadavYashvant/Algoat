c4dbaa311ec18680c1374f2d9482aab20f6ab63c
YadavYashvant <yashvantyadav855@gmail.com>
2024-09-04 18:27:23 +0530

    Initial commit

---
+++ app/src/main/java/com/example/algoat/ui/screens/MainContent.kt
@@ -0,0 +1,13 @@
+package com.example.algoat.ui.screens
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun MainContent(modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello, Algoat!",
+        modifier = modifier
+    )
+}
