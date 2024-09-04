0f603fabba08944f82fbcba2e24f25624a712b0a
YadavYashvant <yashvantyadav855@gmail.com>
2024-09-04 20:48:35 +0530

    fixing some logic

--- app/src/main/java/com/example/algoat/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/algoat/ui/screens/HomeScreen.kt
@@ -5,21 +5,29 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -30,18 +38,24 @@ import com.example.algoat.algorithms.insertionSort
 import com.example.algoat.algorithms.mergeSort
 import com.example.algoat.algorithms.selectionSort
 
-@OptIn(ExperimentalFoundationApi::class)
+@OptIn(ExperimentalFoundationApi::class, ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(innerPadding: PaddingValues, navController: NavController, algorithms: List<SortingAlgorithm>) {
-    var input by remember { mutableStateOf("") }
+
+    var input by remember { mutableStateOf("5,4,6,1,3,7,9,2,8")}
     var times by remember { mutableStateOf<Map<String, Long>>(emptyMap()) }
 
-    Column(modifier = Modifier.padding(innerPadding).padding(horizontal = 16.dp)) {
+    Column(modifier = Modifier.padding(innerPadding).padding(horizontal = 8.dp)) {
         TextField(
             value = input,
             onValueChange = { input = it },
             label = { Text("Input Array (comma separated)") },
             modifier = Modifier.fillMaxWidth().padding(16.dp),
+            shape = MaterialTheme.shapes.medium,
+            colors = TextFieldDefaults.textFieldColors(
+                unfocusedIndicatorColor = Color.Transparent,
+                focusedIndicatorColor = Color.Transparent
+            ),
 
         )
         Button(onClick = {
@@ -62,8 +76,13 @@ fun HomeScreen(innerPadding: PaddingValues, navController: NavController, algori
             times = newTimes
 
         }, modifier = Modifier.padding(8.dp).fillMaxWidth()) {
-            Text("Run", fontSize = 36.sp)
+            Text("Run", fontSize = 24.sp)
         }
+
+        Spacer(modifier = Modifier.height(8.dp))
+        HorizontalDivider(modifier = Modifier.padding(horizontal = 4.dp))
+        Spacer(modifier = Modifier.height(8.dp))
+
         LazyVerticalGrid(
             columns = GridCells.Fixed(2),
             verticalArrangement = Arrangement.spacedBy(8.dp),
@@ -77,9 +96,9 @@ fun HomeScreen(innerPadding: PaddingValues, navController: NavController, algori
                         .clickable { navController.navigate(algorithm.route) }
                 ) {
                     Column(modifier = Modifier.padding(16.dp)) {
-                        Text(text = algorithm.name, fontSize = 24.sp)
+                        Text(text = algorithm.name, fontSize = 32.sp, fontWeight = FontWeight.Bold, lineHeight = 50.sp)
                         times[algorithm.name]?.let { time ->
-                            Text(text = "Time Taken: ${time / 1_000} µs")
+                            Text(text = "Time Taken: ${time / 1_000} µs", fontWeight = FontWeight.W300 )
                         }
                     }
                 }
