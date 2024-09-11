d04512b41bd31d0de61c39fa418c3836bdd900f7
YadavYashvant <yashvantyadav855@gmail.com>
2024-09-11 23:12:25 +0530

    feature: Add new algorithms on your own!

--- app/src/main/java/com/example/algoat/ui/screens/MainContent.kt
+++ app/src/main/java/com/example/algoat/ui/screens/MainContent.kt
@@ -1,6 +1,12 @@
 package com.example.algoat.ui.screens
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.ExtendedFloatingActionButton
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
@@ -47,7 +53,16 @@ fun MainContent(modifier: Modifier = Modifier) {
 
     Scaffold(
         modifier = modifier,
-        bottomBar = { BottomNavigation(navController) }
+        bottomBar = { BottomNavigation(navController) },
+        floatingActionButton = {
+            ExtendedFloatingActionButton(
+                onClick = { navController.navigate("Home") },
+                content = {
+                    Icon(imageVector = Icons.Default.Add, contentDescription = "Add Algorithm")
+                    Text("Add Algorithm")
+                }
+            )
+        }
     ) { innerPadding ->
         NavHost(navController, startDestination = "Home") {
             composable("Home") {
